cd2464203a1829bbd655bd2c9a9fa7451bf6fc72
lilongen <lilongen@gmail.com>
2019-09-11 20:31:33 +0800

    1

--- brswitch.sh
+++ brswitch.sh
@@ -3,42 +3,51 @@
 
 to=${1:-null}
 
+tap_base_default=https://github.com/Homebrew
+tap_base_aliyun=https://mirrors.aliyun.com/homebrew
+tap_base_tsinghua=https://mirrors.tuna.tsinghua.edu.cn/git/homebrew
+
+bottles_default=https://mirrors.aliyun.com/homebrew/homebrew-bottles
+bottles_aliyun=https://mirrors.aliyun.com/homebrew/homebrew-bottles
+bottles_tsinghua=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
+
+
 usage() {
     echo "usage:"
-    echo "$0 %to, %to valid value: default or tsinghua"
+    echo "$0 %to"
+    echo "%to valid value: default, aliyun, tsinghua"
     echo "$0 tsinghua"
     echo "$0 default"
     echo 
 }
 
 regular_to() {
-    if [ $to != "tsinghua" ] && [ $to != "default" ]; then
+    if [ $to != "tsinghua" ] && [ $to != "aliyun" ] && [ $to != "default" ]; then
         usage
         exit 1
     fi
 }
 
-to_tsinghua() {
-    git -C "$(brew --repo)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
-    git -C "$(brew --repo homebrew/core)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
-    git -C "$(brew --repo caskroom/cask)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask.git
-}
-
-to_default() {
-    git -C "$(brew --repo)" remote set-url origin https://github.com/Homebrew/brew.git
-    git -C "$(brew --repo homebrew/core)" remote set-url origin https://github.com/Homebrew/homebrew-core.git
-    git -C "$(brew --repo caskroom/cask)" remote set-url origin https://github.com/Homebrew/homebrew-cask.git
+switch() {
+    tap_base=tap_base_${to}
+    bottles=bottles_${to}
+    url_base=${!tap_base}
+    bottles_url=${!bottles}
+    git -C "$(brew --repo)" remote set-url origin ${url_base}/brew.git
+    git -C "$(brew --repo homebrew/core)" remote set-url origin ${url_base}/homebrew-core.git
+    git -C "$(brew --repo homebrew/cask)" remote set-url origin ${url_base}/homebrew-cask.git
+    echo "export HOMEBREW_BOTTLE_DOMAIN=${bottles_url}" >> ~/.bash_profile
 }
 
 main() {
     regular_to
 
     echo "brew remote set to ${to} ..."
-    to_${to}
+    switch
     echo "brew remote set to ${to} ... done"
-    
+
     echo "brew update ..."
-    brew update
+    brew update -v
     echo "brew update ... done"
 }
 
